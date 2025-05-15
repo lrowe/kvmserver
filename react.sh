75f7923ca3b366e958938d88d191ec9dc4afe793
Laurence Rowe <l@lrowe.co.uk>
2025-05-15 00:08:39 -0700

    Don't use config for react.sh

--- react.sh
+++ react.sh
@@ -1,3 +1,5 @@
-./run.sh -w 1000 -e -t 2 -c deno/config.json -- run --allow-all \
+DENO_V8_FLAGS=--predictable,--stress-maglev,--prepare-always-turbofan,--always-turbofan,--always-sparkplug,--max-old-space-size=256,--max-semi-space-size=256 \
+DENO_NO_UPDATE_CHECK=1 \
+./run.sh --allow-all -w 1000 -e -t 2 -p deno -- run --allow-all \
   https://raw.githubusercontent.com/lrowe/react-server-render-benchmark/refs/heads/main/renderer.mjs \
   "$@"
