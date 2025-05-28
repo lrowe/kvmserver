925c2a08d914cf21825c346a7b240832865d560f
Laurence Rowe <l@lrowe.co.uk>
2025-05-28 15:00:24 -0700

    fix network and add print-config

--- src/config.cpp
+++ src/config.cpp
@@ -10,27 +10,7 @@
 #include <linux/un.h>
 #include <thread>
 #include <unistd.h>
-
-static std::string apply_dollar_vars(std::string str)
-{
-	// Replace $HOME with the home directory
-	auto find_home = str.find("$HOME");
-	if (find_home != std::string::npos) {
-		const char* home = getenv("HOME");
-		if (home != nullptr) {
-			str.replace(find_home, 5, std::string(home));
-		}
-	}
-	// Replace $PWD with the current working directory
-	auto find_pwd = str.find("$PWD");
-	if (find_pwd != std::string::npos) {
-		char cwd[PATH_MAX];
-		if (getcwd(cwd, sizeof(cwd)) != nullptr) {
-			str.replace(find_pwd, 4, std::string(cwd));
-		}
-	}
-	return str;
-}
+#include <ranges>
 
 template <typename T>
 void add_remappings(const nlohmann::json& json, std::vector<T>& remappings,
@@ -103,80 +83,121 @@ std::string lookup_program(const std::string& program)
 }
 
 
-static void parse_address(
-	const std::string_view value,
+// Return value indicates whether to break out of loop
+static bool parse_addresses(
+	const std::vector<std::string>& config,
 	std::vector<struct sockaddr_storage>& allowed_ipv4,
-	std::vector<struct sockaddr_storage>& allowed_ipv6
+	std::vector<struct sockaddr_storage>& allowed_ipv6,
+	const bool verbose
 ) {
-	uint port = 0;
-	std::string address(value);
-	size_t maybe_colon = address.find_last_not_of("0123456789");
-	if (maybe_colon != std::string::npos && value[maybe_colon] == ':') {
-		uint parsed_port;
-		try {
-			port = std::stoul(address.substr(maybe_colon + 1, std::string::npos));
-		} catch (...) {
+	for (const auto& value : config) {
+		if (value.empty() || value == "false") {
+			continue;
+		}
+		uint port = 0;
+		std::string address(value);
+		if (value == "true") {
+			address = "";
+		}
+		size_t maybe_colon = address.find_last_not_of("0123456789");
+		if (maybe_colon != std::string::npos && value[maybe_colon] == ':') {
+			uint parsed_port;
+			try {
+				port = std::stoul(address.substr(maybe_colon + 1, std::string::npos));
+			} catch (...) {
+				throw std::runtime_error(std::format("Invalid port: {}", value));
+			}
+			address = address.substr(0, maybe_colon);
+		}
+
+		if (port > std::numeric_limits<in_port_t>::max()) {
 			throw std::runtime_error(std::format("Invalid port: {}", value));
 		}
-		address = address.substr(0, maybe_colon);
-	}
 
-	if (port > std::numeric_limits<in_port_t>::max()) {
-		throw std::runtime_error(std::format("Invalid port: {}", value));
-	}
-
-	// IPv6
-	if (address.front() == '[') {
-		if (address.back() != ']') {
-			throw std::runtime_error(std::format("Invalid ipv6 address: ", value));
+		if (address == "") {
+			if (port == 0) {
+				allowed_ipv4.clear();
+				allowed_ipv6.clear();
+			}
+			auto& storage = allowed_ipv4.emplace_back((struct sockaddr_storage) {});
+			storage.ss_family = AF_INET;
+			reinterpret_cast<sockaddr_in*>(&storage)->sin_port = htons(port);
+			storage = allowed_ipv6.emplace_back((struct sockaddr_storage) {});
+			storage.ss_family = AF_INET6;
+			reinterpret_cast<sockaddr_in6*>(&storage)->sin6_port = htons(port);
+			if (port == 0) {
+				return true;
+			};
+			continue;
 		}
-		std::string address(address.substr(1, value.length() - 2));
-		auto& storage = allowed_ipv6.emplace_back((struct sockaddr_storage) {});
-		sockaddr_in6* addr = reinterpret_cast<sockaddr_in6*>(&storage);
-		addr->sin6_family = AF_INET6;
-		if (inet_pton(AF_INET6, address.c_str(), &addr->sin6_addr) <= 0) {
-			throw std::runtime_error(std::format("Invalid IPv6 address: {}", value));
+
+		// IPv6
+		if (address.front() == '[') {
+			if (address.back() != ']') {
+				throw std::runtime_error(std::format("Invalid ipv6 address: ", value));
+			}
+			std::string address(address.substr(1, value.length() - 2));
+			auto& storage = allowed_ipv6.emplace_back((struct sockaddr_storage) {});
+			sockaddr_in6* addr = reinterpret_cast<sockaddr_in6*>(&storage);
+			addr->sin6_family = AF_INET6;
+			if (inet_pton(AF_INET6, address.c_str(), &addr->sin6_addr) <= 0) {
+				throw std::runtime_error(std::format("Invalid IPv6 address: {}", value));
+			}
+			addr->sin6_port = htons(port);
+			return false;
 		}
-		addr->sin6_port = htons(port);
-		return;
-	}
 
-	// IPv4
-	in_addr sin_addr;
-	if (inet_pton(AF_INET, address.c_str(), &sin_addr) > 0) {
-		auto& storage = allowed_ipv4.emplace_back((struct sockaddr_storage) {});
-		sockaddr_in* addr = reinterpret_cast<sockaddr_in*>(&storage);
-		addr->sin_family = AF_INET;
-		addr->sin_addr = sin_addr;
-		addr->sin_port = htons(port);
-		return;
-	}
+		// IPv4
+		in_addr sin_addr;
+		if (inet_pton(AF_INET, address.c_str(), &sin_addr) > 0) {
+			auto& storage = allowed_ipv4.emplace_back((struct sockaddr_storage) {});
+			sockaddr_in* addr = reinterpret_cast<sockaddr_in*>(&storage);
+			addr->sin_family = AF_INET;
+			addr->sin_addr = sin_addr;
+			addr->sin_port = htons(port);
+			continue;
+		}
 
-	// Resolve the domain name to an IP address
-	struct addrinfo hints = {};
-	struct addrinfo* res;
-	hints.ai_family = AF_UNSPEC; // IPv4 or IPv6
-	hints.ai_socktype = SOCK_STREAM; // TCP
-	if (getaddrinfo(address.c_str(), nullptr, &hints, &res) != 0) {
-		throw std::runtime_error(std::format("Invalid domain name: {}", value));
+		// Resolve the domain name to an IP address
+		struct addrinfo hints = {};
+		struct addrinfo* head;
+		hints.ai_family = AF_UNSPEC; // IPv4 or IPv6
+		hints.ai_socktype = SOCK_STREAM; // TCP
+		if (getaddrinfo(address.c_str(), nullptr, &hints, &head) != 0) {
+			throw std::runtime_error(std::format("Invalid domain name: {}", value));
+		}
+		for (struct addrinfo* res = head; res != nullptr; res = res->ai_next) {
+			if (res->ai_family == AF_INET) {
+				auto& storage = allowed_ipv4.emplace_back((struct sockaddr_storage) {});
+				sockaddr_in* addr = reinterpret_cast<sockaddr_in*>(&storage);
+				addr->sin_family = AF_INET;
+				addr->sin_addr = reinterpret_cast<sockaddr_in*>(res->ai_addr)->sin_addr;
+				addr->sin_port = htons(port);
+				if (verbose) {
+					std::string found;
+					found.reserve(64);
+					inet_ntop(AF_INET, reinterpret_cast<void*>(&addr->sin_addr), found.data(), found.capacity());
+					printf("Resolved %s to %s\n", address.c_str(), found.c_str());
+				}
+			} else if (res->ai_family == AF_INET6) {
+				auto& storage = allowed_ipv6.emplace_back((struct sockaddr_storage) {});
+				sockaddr_in6* addr = reinterpret_cast<sockaddr_in6*>(&storage);
+				addr->sin6_family = AF_INET6;
+				addr->sin6_addr = reinterpret_cast<sockaddr_in6*>(res->ai_addr)->sin6_addr;
+				addr->sin6_port = htons(port);
+				if (verbose) {
+					std::string found;
+					found.reserve(64);
+					inet_ntop(AF_INET6, reinterpret_cast<void*>(&addr->sin6_addr), found.data(), found.capacity());
+					printf("Resolved %s to %s\n", address.c_str(), found.c_str());
+				}
+			} else {
+				throw std::runtime_error(std::format("Invalid address family for domain: {}", value));
+			}
+		}
+		freeaddrinfo(head);
 	}
-	if (res->ai_family == AF_INET) {
-		auto& storage = allowed_ipv4.emplace_back((struct sockaddr_storage) {});
-		sockaddr_in* addr = reinterpret_cast<sockaddr_in*>(&storage);
-		addr->sin_family = AF_INET;
-		addr->sin_addr = reinterpret_cast<sockaddr_in*>(res)->sin_addr;
-		addr->sin_port = htons(port);
-	} else if (res->ai_family == AF_INET6) {
-		auto& storage = allowed_ipv6.emplace_back((struct sockaddr_storage) {});
-		sockaddr_in6* addr = reinterpret_cast<sockaddr_in6*>(&storage);
-		addr->sin6_family = AF_INET6;
-		addr->sin6_addr = reinterpret_cast<sockaddr_in6*>(res)->sin6_addr;
-		addr->sin6_port = htons(port);
-	} else {
-		throw std::runtime_error(std::format("Invalid address family for domain: {}", value));
-	}
-	freeaddrinfo(res);
-	return;
+	return false;
 }
 
 Configuration Configuration::FromArgs(int argc, char* argv[])
@@ -185,7 +206,7 @@ Configuration Configuration::FromArgs(int argc, char* argv[])
 	CLI::App app{"kvmserver"};
 
 	uint verbose = 0;
-	bool allow_all;
+	bool allow_all = false;
 	bool allow_read = false;
 	bool allow_write = false;
 	std::vector<std::string> allow_env;
@@ -207,32 +228,34 @@ Configuration Configuration::FromArgs(int argc, char* argv[])
 	// -vv include syscalls, -vvv also include memory maps
 	app.add_flag("-v,--verbose", verbose, "Enable verbose output");
 
-	app.add_flag("--allow-all", allow_all, "Allow all access")->group("PERMISSION OPTIONS");
-	app.add_flag("--allow-read", allow_read, "Allow filesystem read access")->group("PERMISSION OPTIONS");
-	app.add_flag("--allow-write", allow_write, "Allow filesystem write access")->group("PERMISSION OPTIONS");
-	app.add_flag("--allow-env{*}", allow_env, "Allow access to environment variables. Optionally specify accessible environment variables (e.g. --allow-env=USER,PATH,API_*).")->group("PERMISSION OPTIONS");
-	app.add_flag("--allow-net{}", allow_all, "Allow network access")->group("PERMISSION OPTIONS");
-	app.add_flag("--allow-connect{}", allow_read, "Allow outgoing network access")->group("PERMISSION OPTIONS");
-	app.add_flag("--allow-listen{}", allow_write, "Allow incoming network access")->group("PERMISSION OPTIONS");
+	app.add_flag("--allow-all", allow_all, "Allow all access")->group("Permissions");
+	app.add_flag("--allow-read", allow_read, "Allow filesystem read access")->group("Permissions");
+	app.add_flag("--allow-write", allow_write, "Allow filesystem write access")->group("Permissions");
+	app.add_flag("--allow-env{*}", allow_env, "Allow access to environment variables. Optionally specify accessible environment variables (e.g. --allow-env=USER,PATH,API_*).")->group("Permissions");
+	app.add_flag("--allow-net", allow_net, "Allow network access")->delimiter(',')->group("Permissions");
+	app.add_flag("--allow-connect", allow_connect, "Allow outgoing network access")->delimiter(',')->group("Permissions");
+	app.add_flag("--allow-listen", allow_listen, "Allow incoming network access")->delimiter(',')->group("Permissions");
 
-	app.add_option("--max-boot-time", config.max_boot_time)->capture_default_str()->group("ADVANCED OPTIONS");
-	app.add_option("--max-request-time", config.max_req_time)->capture_default_str()->group("ADVANCED OPTIONS");
-	app.add_option("--max-main-memory", config.max_main_memory)->capture_default_str()->group("ADVANCED OPTIONS");
-	app.add_option("--max-address-space", config.max_address_space)->capture_default_str()->group("ADVANCED OPTIONS");
-	app.add_option("--max-request-memory", config.max_req_mem)->capture_default_str()->group("ADVANCED OPTIONS");
-	app.add_option("--limit-request-memory", config.limit_req_mem)->capture_default_str()->group("ADVANCED OPTIONS");
-	app.add_option("--shared-memory", config.shared_memory)->capture_default_str()->group("ADVANCED OPTIONS");
-	app.add_option("--dylink-address-hint", config.dylink_address_hint)->capture_default_str()->group("ADVANCED OPTIONS");
-	app.add_option("--heap-address-hint", config.heap_address_hint)->capture_default_str()->group("ADVANCED OPTIONS");
-	app.add_option("--hugepage-arena-size", config.hugepage_arena_size)->capture_default_str()->group("ADVANCED OPTIONS");
-	app.add_option("--hugepage-requests-arena", config.hugepage_requests_arena)->capture_default_str()->group("ADVANCED OPTIONS");
-	app.add_flag("!--no-executable-heap", config.executable_heap)->capture_default_str()->group("ADVANCED OPTIONS");
-	app.add_flag("--clock-gettime-uses-rdtsc", config.clock_gettime_uses_rdtsc)->group("ADVANCED OPTIONS");
-	app.add_flag("--hugepages", config.hugepages)->group("ADVANCED OPTIONS");
-	app.add_flag("!--no-split-hugepages", config.split_hugepages)->group("ADVANCED OPTIONS");
-	app.add_flag("--transparent-hugepages", config.transparent_hugepages)->group("ADVANCED OPTIONS");
-	app.add_flag("!--no-relocate-fixed-mmap", config.relocate_fixed_mmap)->group("ADVANCED OPTIONS");
-	app.add_flag("!--no-ephemeral-keep-working-memory", config.ephemeral_keep_working_memory)->group("ADVANCED OPTIONS");
+	app.add_option("--max-boot-time", config.max_boot_time)->capture_default_str()->group("Advanced");
+	app.add_option("--max-request-time", config.max_req_time)->capture_default_str()->group("Advanced");
+	app.add_option("--max-main-memory", config.max_main_memory)->capture_default_str()->group("Advanced");
+	app.add_option("--max-address-space", config.max_address_space)->capture_default_str()->group("Advanced");
+	app.add_option("--max-request-memory", config.max_req_mem)->capture_default_str()->group("Advanced");
+	app.add_option("--limit-request-memory", config.limit_req_mem)->capture_default_str()->group("Advanced");
+	app.add_option("--shared-memory", config.shared_memory)->capture_default_str()->group("Advanced");
+	app.add_option("--dylink-address-hint", config.dylink_address_hint)->capture_default_str()->group("Advanced");
+	app.add_option("--heap-address-hint", config.heap_address_hint)->capture_default_str()->group("Advanced");
+	app.add_option("--hugepage-arena-size", config.hugepage_arena_size)->capture_default_str()->group("Advanced");
+	app.add_option("--hugepage-requests-arena", config.hugepage_requests_arena)->capture_default_str()->group("Advanced");
+	app.add_flag("!--no-executable-heap", config.executable_heap)->capture_default_str()->group("Advanced");
+	app.add_flag("--clock-gettime-uses-rdtsc", config.clock_gettime_uses_rdtsc)->group("Advanced");
+	app.add_flag("--hugepages", config.hugepages)->group("Advanced");
+	app.add_flag("!--no-split-hugepages", config.split_hugepages)->group("Advanced");
+	app.add_flag("--transparent-hugepages", config.transparent_hugepages)->group("Advanced");
+	app.add_flag("!--no-relocate-fixed-mmap", config.relocate_fixed_mmap)->group("Advanced");
+	app.add_flag("!--no-ephemeral-keep-working-memory", config.ephemeral_keep_working_memory)->group("Advanced");
+
+	CLI::Option* print_config = app.add_flag("--print-config", "Print config and exit without running program");
 
 	try {
 		app.parse(argc, argv);
@@ -240,12 +263,11 @@ Configuration Configuration::FromArgs(int argc, char* argv[])
     std::exit(app.exit(e));
 	}
 
-	std::cout<<app.config_to_str(true,true);
-	/*printf("Environment: %ld [", config.environ.size());
-		for (const auto& env : config.environ) {
-			printf("%s ", env.c_str());
-		}
-		printf("]\n");*/
+	if (*print_config) {
+		app.remove_option(print_config);
+		std::cout<<app.config_to_str(false, true);
+		std::exit(0);
+	}
 
 	if (verbose >= 1) {
 		config.verbose = true;
@@ -263,11 +285,7 @@ Configuration Configuration::FromArgs(int argc, char* argv[])
 		allow_env.clear();
 		allow_env.emplace_back("*");
 		allow_net.clear();
-		allow_net.emplace_back("");
-	}
-	if (!allow_net.empty()) {
-		allow_connect.insert(allow_connect.begin(), allow_net.begin(), allow_net.end());
-		allow_listen.insert(allow_listen.begin(), allow_net.begin(), allow_net.end());
+		allow_net.emplace_back("true");
 	}
 	if (config.concurrency == 0) {
 		config.concurrency = std::thread::hardware_concurrency();
@@ -332,41 +350,27 @@ Configuration Configuration::FromArgs(int argc, char* argv[])
 		config.environ.emplace_back("USER=nobody");
 	}
 
-	for (const auto& values : allow_connect) {
-		if (values == "") {
-			config.allowed_connect_ipv4.clear();
-			auto& storage = config.allowed_connect_ipv4.emplace_back((struct sockaddr_storage) {});
-			storage.ss_family = AF_INET;
-			config.allowed_connect_ipv6.clear();
-			storage = config.allowed_connect_ipv6.emplace_back((struct sockaddr_storage) {});
-			storage.ss_family = AF_INET6;
-			break;
-		}
-		for (const auto value : std::views::split(values, ',')) {
-			parse_address(
-				std::string_view(value.begin(), value.end()),
-				config.allowed_connect_ipv4,
-				config.allowed_connect_ipv6
-			);
-		}	
-	}
-	for (const auto& values : allow_listen) {
-		if (values == "") {
-			config.allowed_listen_ipv4.clear();
-			auto& storage = config.allowed_listen_ipv4.emplace_back((struct sockaddr_storage) {});
-			storage.ss_family = AF_INET;
-			config.allowed_listen_ipv6.clear();
-			storage = config.allowed_listen_ipv6.emplace_back((struct sockaddr_storage) {});
-			storage.ss_family = AF_INET6;
-			break;
-		}
-		for (const auto value : std::views::split(values, ',')) {
-			parse_address(
-				std::string_view(value.begin(), value.end()),
-				config.allowed_listen_ipv4,
-				config.allowed_listen_ipv6
-			);
-		}	
+	bool skip_allow_connect_listen = parse_addresses(
+		allow_net,
+		config.allowed_connect_ipv4,
+		config.allowed_connect_ipv6,
+		config.verbose
+	);
+	config.allowed_listen_ipv4 = config.allowed_connect_ipv4;
+	config.allowed_listen_ipv6 = config.allowed_connect_ipv6;
+	if (!skip_allow_connect_listen) {
+		parse_addresses(
+			allow_connect,
+			config.allowed_connect_ipv4,
+			config.allowed_connect_ipv6,
+			config.verbose
+		);
+		parse_addresses(
+			allow_listen,
+			config.allowed_listen_ipv4,
+			config.allowed_listen_ipv6,
+			config.verbose
+		);
 	}
 	/*
 	// Print some configuration values
