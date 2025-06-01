fd8e5c23c32d4bf1abc29b67524a912908d5f30b
Alf-André Walla <fwsgonzo@hotmail.com>
2025-06-01 17:12:30 +0200

    Update scripts

--- benchmark.sh
+++ benchmark.sh
@@ -1,2 +1,3 @@
-./run.sh -w 1 -t 4 -e -c deno/kvmserver.toml -- deno serve --allow-all --unstable-kv deno/main.ts
-#./run.sh -w 500 -t 2 -e -c deno/config.json -- run --allow-all https://raw.githubusercontent.com/lrowe/react-server-render-benchmark/refs/heads/main/renderer.mjs
+pushd deno
+source ../run.sh -w 1 -t 4 -e
+popd
--- run.sh
+++ run.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+BUILD_DIR="$SCRIPT_DIR/.build"
 set -e
-mkdir -p .build
-pushd .build
-cmake .. -DCMAKE_BUILD_TYPE=Release
+git submodule update --init
+mkdir -p "$BUILD_DIR"
+pushd "$BUILD_DIR"
+cmake $SCRIPT_DIR -DCMAKE_BUILD_TYPE=Release
 make -j 8
 popd
 
 # When PERF=1 run with perf
 if [ "$PERF" = "1" ]; then
-	perf record -g --call-graph dwarf -- .build/kvmserver "$@"
+	perf record -g --call-graph dwarf -- $BUILD_DIR/kvmserver "$@"
 	perf script | c++filt > perf.out
 	echo "perf.out generated"
 	exit 0
 elif [ "$GDB" = "1" ]; then
 	# When GDB=1 run with gdb
-	gdb --args .build/kvmserver "$@"
+	gdb --args $BUILD_DIR/kvmserver "$@"
 	exit 0
 else
-	./.build/kvmserver "$@"
+	$BUILD_DIR/kvmserver "$@"
 fi
