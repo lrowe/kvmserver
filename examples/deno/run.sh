93d18aa697aa3847c44e8eb03f77c496b27a8ae6
Alf-André Walla <fwsgonzo@hotmail.com>
2025-09-22 08:03:42 +0200

    Update Storage example run script

--- examples/deno/run.sh
+++ examples/deno/run.sh
@@ -2,4 +2,7 @@ set -e
 pushd ../../.build
 make -j8
 popd
+
+export DENO_V8_FLAGS=--predictable,--max-old-space-size=64,--max-semi-space-size=64
+export DENO_NO_UPDATE_CHECK=1
 ../../.build/kvmserver -e --storage --allow-all ~/.deno/bin/deno -- run --allow-all local.ts
