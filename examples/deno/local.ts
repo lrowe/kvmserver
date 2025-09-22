3bfcc21b54ce89b97be8ea2d86a43d52fdb47110
Laurence Rowe <l@lrowe.co.uk>
2025-09-21 20:06:01 -0700

    Fix linter errors

--- examples/deno/local.ts
+++ examples/deno/local.ts
@@ -1,29 +1,35 @@
 console.log("Hello from Deno inside TinyKVM");
-const drogon = Deno.dlopen("/home/gonzo/github/kvmserver/examples/deno/liblocal.so", {
-	remote_resume: { parameters: ["buffer", "usize"], result: "void" },
-});
-function getZeroTerminatedString(buffer, encoding = 'utf8') {
-	const nullByteIndex = buffer.indexOf(0x00);
-	const slice = nullByteIndex !== -1 ? buffer.slice(0, nullByteIndex) : buffer;
-	return new TextDecoder(encoding).decode(slice);
+const drogon = Deno.dlopen(
+  import.meta.resolve("liblocal.so"),
+  {
+    remote_resume: { parameters: ["buffer", "usize"], result: "void" },
+  },
+);
+function getZeroTerminatedString(buffer: Uint8Array, encoding = "utf8") {
+  const nullByteIndex = buffer.indexOf(0x00);
+  const slice = nullByteIndex !== -1 ? buffer.slice(0, nullByteIndex) : buffer;
+  return new TextDecoder(encoding).decode(slice);
 }
 function getRemoteString(): string {
-	const remote_buffer = new Uint8Array(256);
-	drogon.symbols.remote_resume(remote_buffer, remote_buffer.byteLength);
+  const remote_buffer = new Uint8Array(256);
+  drogon.symbols.remote_resume(remote_buffer, BigInt(remote_buffer.byteLength));
 
-	// Get remote_buffer as a zero-terminated string
-	return getZeroTerminatedString(remote_buffer);
+  // Get remote_buffer as a zero-terminated string
+  return getZeroTerminatedString(remote_buffer);
 }
 
 // Simple HTTP server
-Deno.serve({ port: 8080 }, async (req) => {
-	const url = new URL(req.url);
-	if (url.pathname === "/") {
-		const remote_str = getRemoteString();
-		return new Response("Hello from Deno inside TinyKVM\n" + remote_str + "\n", {
-			headers: { "content-type": "text/plain; charset=utf-8" },
-		});
-	} else {
-		return new Response("Not Found\n", { status: 404 });
-	}
+Deno.serve({ port: 8080 }, (req) => {
+  const url = new URL(req.url);
+  if (url.pathname === "/") {
+    const remote_str = getRemoteString();
+    return new Response(
+      "Hello from Deno inside TinyKVM\n" + remote_str + "\n",
+      {
+        headers: { "content-type": "text/plain; charset=utf-8" },
+      },
+    );
+  } else {
+    return new Response("Not Found\n", { status: 404 });
+  }
 });
--- examples/deno/remote.ts
+++ examples/deno/remote.ts
@@ -1,27 +1,34 @@
-import { connect } from "jsr:@db/redis";
+import { connect } from "@db/redis";
 
-const drogon = Deno.dlopen("/home/gonzo/github/kvmserver/examples/deno/libstorage.so", {
-	wait_for_storage_task_paused: { parameters: [], result: "pointer" },
-});
+const drogon = Deno.dlopen(
+  import.meta.resolve("./libstorage.so"),
+  {
+    wait_for_storage_task_paused: { parameters: [], result: "pointer" },
+  },
+);
 console.log("Hello from Deno Storage inside TinyKVM");
 
 const redisClient = await connect({ hostname: "127.0.0.1", port: 6379 });
 while (true) {
-	// Wait for a UInt8Array buffer from C
-	const bufptr = drogon.symbols.wait_for_storage_task_paused();
-	// View it as a Uint8Array of length 256
-	const arrayBuffer = Deno.UnsafePointerView.getArrayBuffer(bufptr, 256);
-	const buffer = new Uint8Array(arrayBuffer);
+  // Wait for a UInt8Array buffer from C
+  const bufptr = drogon.symbols.wait_for_storage_task_paused();
+  if (bufptr === null) {
+    throw new Error("bufptr is null");
+  }
+  // View it as a Uint8Array of length 256
+  const arrayBuffer = Deno.UnsafePointerView.getArrayBuffer(bufptr, 256);
+  const buffer = new Uint8Array(arrayBuffer);
 
-	const redis_answer = await redisClient.get("hoge");
-	// Copy redis_answer to buffer
-	const response = "Hello from Deno storage inside TinyKVM, redis answer: " + redis_answer;
-	const encoder = new TextEncoder();
-	let encoded = encoder.encode(response);
-	// Copy to buffer, but leave space for null terminator
-	if (encoded.length > 255) {
-		encoded = encoded.slice(0, 255);
-	}
-	buffer.set(encoded); // Leave space for null terminator
-	buffer[encoded.length] = 0; // Null-terminate
+  const redis_answer = await redisClient.get("hoge");
+  // Copy redis_answer to buffer
+  const response = "Hello from Deno storage inside TinyKVM, redis answer: " +
+    redis_answer;
+  const encoder = new TextEncoder();
+  let encoded = encoder.encode(response);
+  // Copy to buffer, but leave space for null terminator
+  if (encoded.length > 255) {
+    encoded = encoded.slice(0, 255);
+  }
+  buffer.set(encoded); // Leave space for null terminator
+  buffer[encoded.length] = 0; // Null-terminate
 }
