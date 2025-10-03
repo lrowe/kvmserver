c9628c36f8c52db141567604bd1a9a707c5aa53b
Alf-André Walla <fwsgonzo@hotmail.com>
2025-10-03 13:59:47 +0200

    Update TinyKVM

--- examples/deno/run.sh
+++ examples/deno/run.sh
@@ -5,4 +5,5 @@ popd
 
 export DENO_V8_FLAGS=--predictable,--max-old-space-size=64,--max-semi-space-size=64
 export DENO_NO_UPDATE_CHECK=1
+export DENO_DIR=.deno_cache
 exec ../../.build/kvmserver -e -t 1 --warmup 1000 --allow-all storage --1-to-1 deno run --allow-all remote.ts ++ run deno run --allow-all local.ts
